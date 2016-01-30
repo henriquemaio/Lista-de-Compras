84391e42a269a0d89a179b7f6dc3ef6e91a1deb8
Henrique Maio <henriquemaio@poli.ufrj.br>
2016-01-30 21:28:53 +0100

    Testando com sleep

--- index.php
+++ index.php
@@ -1,8 +1,20 @@
 <?php
-header("Location: index.html");
+
 
 $teste = getenv('TEST');
 
+$db_user = getenv('DB_USER');
+$db_name = getenv('DB_NAME');
+$db_host = getenv('DB_HOST');
+$db_pass = getenv('DB_PASS');
+
+
+
 echo("<script>console.log('PHP: ".json_encode($teste)."');</script>");
+echo("<script>console.log('PHP: ".json_encode($teste)."');</script>");
+
+sleep(10);
+
+header("Location: index.html");
 
 ?>
